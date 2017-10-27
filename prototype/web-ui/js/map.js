8af348fb85015f2d6e30f8fe34475dbd9e775722
Nazar <nazarko1805@gmail.com>
2017-10-28 00:22:09 +0300

    [^] Shift+Enter fix

--- prototype/web-ui/js/map.js
+++ prototype/web-ui/js/map.js
@@ -1,5 +1,37 @@
 var map;
 
+function getCaret(el) {
+    if (el.selectionStart) {
+        return el.selectionStart;
+    } else if (document.selection) {
+        el.focus();
+        var r = document.selection.createRange();
+        if (r == null) {
+            return 0;
+        }
+        var re = el.createTextRange(),
+            rc = re.duplicate();
+        re.moveToBookmark(r.getBookmark());
+        rc.setEndPoint('EndToStart', re);
+        return rc.text.length;
+    }
+    return 0;
+}
+
+$('textarea').keyup(function(event) {
+    if (event.keyCode == 13) {
+        var content = this.value;
+        var caret = getCaret(this);
+        if (event.shiftKey) {
+            this.value = content.substring(0, caret - 1) + "\n" + content.substring(caret, content.length);
+            event.stopPropagation();
+        } else {
+            this.value = content.substring(0, caret - 1) + content.substring(caret, content.length);
+            $('form').submit();
+        }
+    }
+});
+
 document.getElementById("text")
     .addEventListener("keyup", function(event) {
         event.preventDefault();
@@ -23,6 +55,7 @@ function initMap() {
         zoom: 14,
         width: 100
     });
+
 
 
 
