fb30f3f4c0c42ad5586cc817782da9ae24b8db89
Vova0199 <vova0143@gmail.com>
2017-10-27 20:32:47 +0300

    Add click marker

--- prototype/web-ui/js/map.js
+++ prototype/web-ui/js/map.js
@@ -8,12 +8,38 @@ document.getElementById("text")
         }
     });
 
+    var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Фут</h1>'+
+            '<div id="bodyContent">'+
+            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'sandstone rock formation in the southern part of the '+
+            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+            'south west of the nearest large town, Alice Springs; 450&#160;km '+
+            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+            'Aboriginal people of the area. It has many springs, waterholes, '+
+            'rock caves and ancient paintings. Uluru is listed as a World '+
+            'Heritage Site.</p>'+
+            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+            '(last visited June 22, 2009).</p>'+
+            '</div>'+
+            '</div>';
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 14
     });
+
+
+
     var infoWindow = new google.maps.InfoWindow({ map: map });
+
+
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -33,6 +59,21 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+    var infowindow = new google.maps.InfoWindow({
+             content: contentString
+           });
+    var marker = new google.maps.Marker({
+      position: {lat: 49.842560, lng:  24.035905},
+      map: map,
+      title: 'Legalizuem.ru',
+      icon: {
+        url: "img//ball.svg",
+        scaledSize: new google.maps.Size(32,32)
+      }
+    });
+    marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -40,14 +81,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setContent(browserHasGeolocation ?
         'Error: The Geolocation service failed.' :
         'Error: Your browser doesn\'t support geolocation.');
-    var marker = new google.maps.Marker({
-        position: { lat: 49.842401, lng: 24.03599 },
-        map: map,
-        icon: {
-            url: "img/ball.svg",
-            scaledSize: new google.maps.Size(32, 32)
-        }
-    });
+
 
 
     // Create markers.
